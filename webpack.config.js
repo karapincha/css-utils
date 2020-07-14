78dcf7aea321d6ccefbc463dbdbab7b944a93d1b
Kasun Peiris <me@kasun.io>
2020-07-14 19:34:30 +0530

    Added spacer utility

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+	mode: "development",
+	entry: {
+		spacer: "./src/scss/utils/spacer.scss",
+	},
+	output: {
+		filename: "[name].js",
+		path: path.resolve(__dirname, "build"),
+	},
+	watch: true,
+	module: {
+		rules: [
+			{
+				test: /\.s?css$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							esModule: true,
+						},
+					},
+					{
+						loader: "css-loader",
+						options: {
+							importLoaders: 1,
+						},
+					},
+					"sass-loader",
+				],
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+			chunkFilename: "[id].css",
+		}),
+	],
+};
